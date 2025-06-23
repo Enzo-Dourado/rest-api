69f7faa09ba62ab8c3f9ea461aae910b94eafcf5
Enzo Dourado <enzodourado.cripto@gmail.com>
2025-06-23 19:45:49 -0300

    Adicionado Listagem de Médicos e Pacientes

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.DadosMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -24,5 +28,11 @@ public class MedicosController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosMedicos dadosMedicos) {
         repository.save(new Medico(dadosMedicos));
+    }
+
+
+    @GetMapping
+    public List<DadosListagemMedico> listarMedico() {
+        return repository.findAll().stream().map(DadosListagemMedico:: new).toList();
     }
 }
--- src/main/java/med/voll/api/controller/PacientesController.java
+++ src/main/java/med/voll/api/controller/PacientesController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.pacientes.DadosListagemPaciente;
 import med.voll.api.pacientes.DadosPaciente;
 import med.voll.api.pacientes.Paciente;
 import med.voll.api.pacientes.PacientesRepository;
@@ -26,4 +30,8 @@ public class PacientesController {
         repository.save(new Paciente(dadosPaciente));
     }
 
+    @GetMapping
+    public List<DadosListagemPaciente> listarPacientes() {
+        return repository.findAll().stream().map(DadosListagemPaciente:: new).toList();
+    }
 }
