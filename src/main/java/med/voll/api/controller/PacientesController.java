70bd9468e1e42af5fa67be8b206a7b9cb682e209
Enzo Dourado <enzodourado.cripto@gmail.com>
2025-06-23 19:16:51 -0300

    Criando DB de Pacientes e também o recebimento de dados deles

--- src/main/java/med/voll/api/controller/PacientesController.java
+++ src/main/java/med/voll/api/controller/PacientesController.java
@@ -1,20 +1,29 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.pacientes.DadosPaciente;
+import med.voll.api.pacientes.Paciente;
+import med.voll.api.pacientes.PacientesRepository;
 
 @RestController
 @RequestMapping("/pacientes")
 
 public class PacientesController {
     
+    @Autowired
+    private PacientesRepository repository;
+
     @PostMapping
-    public void cadastroPaciente(@RequestBody DadosPaciente dadosPaciente) {
-        System.out.println(dadosPaciente);
+    @Transactional
+    public void cadastroPaciente(@RequestBody @Valid DadosPaciente dadosPaciente) {
+        repository.save(new Paciente(dadosPaciente));
     }
 
 }
--- src/main/java/med/voll/api/pacientes/DadosPaciente.java
+++ src/main/java/med/voll/api/pacientes/DadosPaciente.java
@@ -5,8 +5,8 @@ import med.voll.api.endereco.Endereco;
 public record DadosPaciente(
     String nome,
     String email,
-    long telefone, 
-    long cpf,
+    String telefone, 
+    String cpf,
     Endereco endereco) {
 
 }
