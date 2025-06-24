a96f0ec784c1cd37ee849f0ce34785d2907ed946
Enzo Dourado <enzodourado.cripto@gmail.com>
2025-06-24 12:28:39 -0300

    Criando metodo de atualização de Dados de Médico

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +36,12 @@ public class MedicosController {
     @GetMapping
     public Page<DadosListagemMedico> listarMedico(@PageableDefault(size = 10, sort = {"nome"}) Pageable pagina) {
         return repository.findAll(pagina).map(DadosListagemMedico:: new);
+    }
+
+
+    @PutMapping
+    @Transactional
+    public void atualizacaoMedico(@RequestBody @Valid Dados) {
+
     }
 }
